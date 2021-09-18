6077d29d45439e107a896a2057cd9379c122de02
NPComplete <dwight@dwightjbrowne.com>
2021-09-17 21:34:17 -0600

    working version 005

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::net::{TcpStream};
 use std::io::{Read, Write};
 use std::str::from_utf8;
+
 fn main() {
     const K_PORT: i32 = 3333;
     const K_HOST: &str = "localhost";
-    const K_BUFSIZ:usize = 21;
+    const K_BUFSIZ:usize = 1024;
+    const K_ITERATIONS: i32 = 10;
     let connect_string = format!("{}:{}",K_HOST ,K_PORT);
-    let mut i = 0;
-    while i < 40 {
+    let mut ctr = 0;
+    while ctr < K_ITERATIONS {
         match TcpStream::connect(&connect_string) {
             Ok(mut stream) => {
-                println!("Iteration {}: Successfully connected to server on port 3333",i);
+                println!("Iteration {}: Successfully connected to server on port 3333",ctr);
                 let mut send_string = Vec::new();
-                std::write!(&mut send_string,"this is message #>{:2}<",i).unwrap();
+                std::write!(&mut send_string,"this is message #>{:25}<",ctr).unwrap();
                 println!("sending {}",from_utf8(&send_string).unwrap());
                 match stream.write(&send_string){
                     Ok(n) => {
@@ -33,10 +35,10 @@ fn main() {
 
                 println!("Sent {} Waiting for reply....",from_utf8(&send_string).unwrap());
                 let mut data = [0 as u8; K_BUFSIZ];
-                match stream.read_exact(&mut data) {
+                match stream.read(&mut data) {
                     Ok(n) => {
-                        if &data[..] == &send_string[..] {
-                            println!("reply is ok! read {} bytes",K_BUFSIZ);
+                        if &data[..n] == &send_string[..] {
+                            println!("reply is ok! read {} bytes",n);
                         } else {
                             let text = from_utf8(&data).unwrap();
                             println!("Unexpected reply: {}", text);
@@ -51,7 +53,7 @@ fn main() {
                 println!("Failed to connect: {}!", e);
             }
         }
-        i +=1;
+        ctr +=1;
     }
     println!("Terminated.");
 }
