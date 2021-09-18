739a341e6371c051a428977de32924f22f567f7c
NPComplete <dwight@dwightjbrowne.com>
2021-09-17 20:16:11 -0600

    Working version 002.

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,27 @@ use std::net::{TcpStream};
 use std::io::{Read, Write};
 use std::str::from_utf8;
 fn main() {
-    println!("hit it!");
     const K_PORT: i32 = 3333;
-    const K_SIZE: usize = 1024;
     let connect_string = format!("localhost:{}", K_PORT);
     let mut i = 0;
-    while i < 20 {
+    while i < 40 {
         match TcpStream::connect(&connect_string) {
             Ok(mut stream) => {
                 println!("Iteration {}: Successfully connected to server on port 3333",i);
                 let mut send_string = Vec::new();
                 std::write!(&mut send_string,"this is message #>{:2}<",i).unwrap();
                 println!("sending {}",from_utf8(&send_string).unwrap());
-                let msg = b"hello!";
-                stream.write(&send_string).unwrap();
+                match stream.write(&send_string){
+                    Ok(_) => {
+                        println!("write ok!");
+                    },
+                    Err(e) =>{
+                        println!("Canna write the data Captn'! {}", e);
+                    }
+                }
+                stream.flush().unwrap();
+
+
                 println!("Sent {} Waiting for reply....",from_utf8(&send_string).unwrap());
                 let mut data = [0 as u8; 21];
                 match stream.read_exact(&mut data) {
