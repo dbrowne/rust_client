35178e0b5635afab6ceee9b98edc848578a1d9bb
NPComplete <dwight@dwightjbrowne.com>
2021-09-17 22:38:25 -0600

    output changes

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     const K_PORT: i32 = 3333;
     const K_HOST: &str = "localhost";
     const K_BUFSIZ:usize = 1024;
-    const K_ITERATIONS: i32 = 10;
+    const K_ITERATIONS: i32 = 20;
     let connect_string = format!("{}:{}",K_HOST ,K_PORT);
     let mut ctr = 0;
     while ctr < K_ITERATIONS {
@@ -15,7 +15,7 @@ fn main() {
             Ok(mut stream) => {
                 println!("{}: iteration {}: Successfully connected to server on port 3333",Utc::now(), ctr);
                 let mut send_string = Vec::new();
-                std::write!(&mut send_string,"{}: v this is message #>{:25}<",Utc::now(),   ctr).unwrap();
+                std::write!(&mut send_string,"{}: --------from {} message #>{}<",Utc::now(),K_HOST,   ctr).unwrap();
                 println!("{}: sending {}",Utc::now(),from_utf8(&send_string).unwrap());
                 match stream.write(&send_string){
                     Ok(n) => {
@@ -32,8 +32,6 @@ fn main() {
                         println!("{}: Canna flush it {}",Utc::now(), e);
                     }
                 }
-
-
                 println!("{}: Sent {} Waiting for reply....",Utc::now(), from_utf8(&send_string).unwrap());
                 let mut data = [0 as u8; K_BUFSIZ];
                 match stream.read(&mut data) {
