c9cc759acc84c7c781c218402fbd899c6a8471df
NPComplete <dwight@dwightjbrowne.com>
2021-09-17 21:58:50 -0600

    Added Time stamp and chrono

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::{TcpStream};
 use std::io::{Read, Write};
 use std::str::from_utf8;
+use chrono::{DateTime, Utc, Duration};
 
 fn main() {
     const K_PORT: i32 = 3333;
@@ -12,48 +13,48 @@ fn main() {
     while ctr < K_ITERATIONS {
         match TcpStream::connect(&connect_string) {
             Ok(mut stream) => {
-                println!("Iteration {}: Successfully connected to server on port 3333",ctr);
+                println!("{}: iteration {}: Successfully connected to server on port 3333",Utc::now(), ctr);
                 let mut send_string = Vec::new();
-                std::write!(&mut send_string,"this is message #>{:25}<",ctr).unwrap();
-                println!("sending {}",from_utf8(&send_string).unwrap());
+                std::write!(&mut send_string,"{}: v this is message #>{:25}<",Utc::now(),   ctr).unwrap();
+                println!("{}: sending {}",Utc::now(),from_utf8(&send_string).unwrap());
                 match stream.write(&send_string){
                     Ok(n) => {
-                        println!("write ok! {} bytes",n);
+                        println!("{}: write ok! {} bytes",Utc::now(),n);
                     },
                     Err(e) =>{
-                        println!("Canna write the data Captn'! {}", e);
+                        println!("{}: Canna write the data Captn'! {}",Utc::now(), e);
                     }
                 }
 
                 match stream.flush(){
                     Ok(_) =>{},
                     Err(e) =>{
-                        println!("Canna flush it {}",e);
+                        println!("{}: Canna flush it {}",Utc::now(), e);
                     }
                 }
 
 
-                println!("Sent {} Waiting for reply....",from_utf8(&send_string).unwrap());
+                println!("{}: Sent {} Waiting for reply....",Utc::now(), from_utf8(&send_string).unwrap());
                 let mut data = [0 as u8; K_BUFSIZ];
                 match stream.read(&mut data) {
                     Ok(n) => {
                         if &data[..n] == &send_string[..] {
-                            println!("reply is ok! read {} bytes",n);
+                            println!("{}: reply is ok! read {} bytes",Utc::now(), n);
                         } else {
                             let text = from_utf8(&data).unwrap();
-                            println!("Unexpected reply: {}", text);
+                            println!("{}: Unexpected reply: {}",Utc::now(), text);
                         }
                     },
                     Err(e) => {
-                        println!("failed to receive data {}", e);
+                        println!("{}: failed to receive data {}",Utc::now(), e);
                     }
                 }
             },
             Err(e) => {
-                println!("Failed to connect: {}!", e);
+                println!("{}: Failed to connect: {}!",Utc::now(), e);
             }
         }
         ctr +=1;
     }
-    println!("Terminated.");
+    println!("{}: Terminated.",Utc::now());
 }
