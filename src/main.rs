a4f8ad48c40a8a5ae6834c8329bedd8338e12aac
NPComplete <dwight@dwightjbrowne.com>
2021-09-18 22:30:45 -0600

    added input args

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,51 @@ use std::io::{Read, Write};
 use std::str::from_utf8;
 use chrono::Utc;
 use std::{thread, time};
+use structopt::StructOpt;
 
 fn main() {
-    const K_PORT: i32 = 3333;
+    const K_PORT: &str = "3333";
     const K_HOST: &str = "localhost";
-    const K_BUFSIZ:usize = 1024;
-    const K_ITERATIONS: i32 = 20;
-    const K_DURATION: u64 = 5;
+    const K_BUFSIZ: usize = 1024;
+    const K_ITERATIONS: &str = "20";
+    const K_DURATION: &str = "5";
 
-    let connect_string = format!("{}:{}",K_HOST ,K_PORT);
     let mut ctr = 0;
-    while ctr < K_ITERATIONS {
-        thread::sleep(time::Duration::from_secs(K_DURATION));
+// adding arg parsing
+
+    #[derive(StructOpt, Debug)]
+    // #[derive(Debug)]
+    #[structopt(rename_all = "kebab-case")]
+    struct Opt {
+        #[structopt(default_value = K_PORT, short)]
+        port: i32,
+
+        #[structopt(default_value = K_HOST, short)]
+        host: String,
+
+        #[structopt(default_value = K_ITERATIONS, short)]
+        iter: i32,
+
+        #[structopt(default_value = K_DURATION, short)]
+        dur: u64,
+    }
+
+    let opt = Opt::from_args();
+    println!("{:#?}", &opt);
+
+    let connect_string = format!("{}:{}", opt.host, opt.port);
+
+    while ctr < opt.iter {
+        thread::sleep(time::Duration::from_secs(opt.dur));
         match TcpStream::connect(&connect_string) {
             Ok(mut stream) => {
-                println!("{}: iteration {}: Successfully connected to server on port 3333",Utc::now(), ctr);
+                println!("{}: iteration {}: Successfully connected to server on port 3333", Utc::now(), ctr);
                 let mut send_string = Vec::new();
-                std::write!(&mut send_string,"{}: --------from {} message #>{}<",Utc::now(),K_HOST,   ctr).unwrap();
-                println!("{}: sending {}",Utc::now(),from_utf8(&send_string).unwrap());
-                match stream.write(&send_string){
+                std::write!(&mut send_string, "{}: --------from {} message #>{}<", Utc::now(), K_HOST, ctr).unwrap();
+                println!("{}: sending {}", Utc::now(), from_utf8(&send_string).unwrap());
+                match stream.write(&send_string) {
                     Ok(n) => {
-                        println!("{}: write ok! {} bytes",Utc::now(),n);
+                        println!("{}: write ok! {} bytes", Utc::now(), n);
                     },
                     Err(e) =>{
                         println!("{}: Canna write the data Captn'! {}",Utc::now(), e);
